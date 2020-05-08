2438e2d11f331b2c796e35bcfa119689baa55617
Shin Haneul <s2018s08@e-mirim.hs.kr>
2020-05-08 15:29:25 +0900

    지렁이 움직임

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csloop
@@ -30,6 +31,21 @@ namespace csloop
                 Console.WriteLine(item);
             }
 
-        }
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100);
+                x++;
+            }
     }
 }
