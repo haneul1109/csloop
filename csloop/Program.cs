67cd595d45ca257b7d67d32a2a75106c43ef39b9
Shin Haneul <s2018s08@e-mirim.hs.kr>
2020-05-08 12:07:16 +0900

    for each 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -18,6 +18,18 @@ namespace csloop
                 count++;
             }
             Console.WriteLine(count + "만큼 반복");
+
+            String[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
